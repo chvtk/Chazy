8978499245024f5c1a07347aa5cdda1a4c934016
Chantal <test@test>
2016-04-25 12:43:31 -0600

    deleted old SOM files

--- ssu_aln.ssu-align.sh
+++ ssu_aln.ssu-align.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Bash shell script created by ssu-prep for running 20 ssu-align jobs.
 # Each job will process a separate partition of the sequence file:
-# 'data/otusn.pick.fasta'.
+# 'otusn.pick.fasta'.
 #
 # This script will execute all 20 jobs at once, in parallel. It is only
 # meant to be executed on a system with 20 cpus/cores. The first 19 jobs
@@ -10,15 +10,15 @@
 #
 # The final job is special, after computing its alignments it will wait for all
 # other jobs to finish and then merge the output of all jobs together.
-# The merged output files will be in the directory: '/data/tmp/ssu_aln/'
+# The merged output files will be in the directory: '/ssu_aln/'
 #
 # The for loop below will execute/submit the first 19 of 20 jobs.
 # The final ssu-align job is executed separately because it does the merging.
 #
 for (( i=1; i<=19; i++ ))
 do
-	echo "# Executing: ssu-align -b 50 --dna --rfonly data/tmp/ssu_aln/otusn.pick.fasta.$i data/tmp/ssu_aln/ssu_aln.$i > /dev/null &"
-	ssu-align -b 50 --dna --rfonly data/tmp/ssu_aln/otusn.pick.fasta.$i data/tmp/ssu_aln/ssu_aln.$i > /dev/null &
+	echo "# Executing: ssu-align -b 50 --dna --rfonly ssu_aln/otusn.pick.fasta.$i ssu_aln/ssu_aln.$i > /dev/null &"
+	ssu-align -b 50 --dna --rfonly ssu_aln/otusn.pick.fasta.$i ssu_aln/ssu_aln.$i > /dev/null &
 done
-echo "# Executing: ssu-align --merge 20 -b 50 --dna --rfonly data/tmp/ssu_aln/otusn.pick.fasta.20 data/tmp/ssu_aln/ssu_aln.20"
-ssu-align --merge 20 -b 50 --dna --rfonly data/tmp/ssu_aln/otusn.pick.fasta.20 data/tmp/ssu_aln/ssu_aln.20
+echo "# Executing: ssu-align --merge 20 -b 50 --dna --rfonly ssu_aln/otusn.pick.fasta.20 ssu_aln/ssu_aln.20"
+ssu-align --merge 20 -b 50 --dna --rfonly ssu_aln/otusn.pick.fasta.20 ssu_aln/ssu_aln.20
